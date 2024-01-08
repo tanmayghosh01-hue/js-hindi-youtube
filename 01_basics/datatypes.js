a83741a1e5bcb5d40ecf3554d4c62895f5771cf1
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-08 10:26:50 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello") we are using node js not browser
+
+let isLoggedIn = false
+let state;
+
+
+// number => to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
+
+
+
+
+
+
+
+
