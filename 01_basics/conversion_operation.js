9bb723c757d280aa3ba26d49efd79d3ae568d8d3
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-08 18:37:55 +0000

    Conversion of data types

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,22 @@
+let score = "33abc" // NaN -> Not a Number
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber)
+
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
