02b6f141313520537f1fd9f42aa39b4a9cef3bac
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-08 09:42:21 +0000

    class 3

---
+++ 01_basics/Variables.js
@@ -0,0 +1,7 @@
+const accountId = "14234"
+let accountName = "Shouraya Adam"
+var CompanyName = "Jarvis"
+favorate_fruit = "Carrot";
+let favoriteSport;
+
+console.table([accountId, accountName, CompanyName, favorate_fruit, favoriteSport])
