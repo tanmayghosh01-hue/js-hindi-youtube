f1b84112c477aff56b11aa468bcddac876fdc40c
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-09 13:19:54 +0000

    Operations done

---
+++ 01_basics/Operations.js
@@ -0,0 +1,26 @@
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+//*************Operations*********** */
+
+let value = 3
+
+let negValue = -value
+
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2
+
+console.log(str3)
+
+console.log(+true)
+
+console.log(+"")
+
