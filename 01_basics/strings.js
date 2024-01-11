3e1d58b33dc8e34b333c5b02f7187a3d00c5b643
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-11 08:37:14 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,40 @@
+const name = "tanmay"
+
+const repoCount = 50
+
+// console.log(name + repoCount + " Value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Tanmay-G')
+// console.log(gameName[1])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+
+console.log(gameName.indexOf('y'))
+
+
+const newString = gameName.substring(0, 4)
+
+console.log(newString)
+
+const anotherString = gameName.slice(-5, 2)
+console.log(anotherString)
+
+
+const newStringOne = "   Boheman    "
+
+
+console.log(newStringOne.trim())
+
+const url = "https://youtube.com"
+
+console.log(url.replace('ube', '_'))
+
+console.log(url.includes('https'))
+
+console.log(gameName.split)
+console.log(url.split(':'))
