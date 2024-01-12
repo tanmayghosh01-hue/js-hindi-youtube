8ea98219a70859bdfeb0bde269c0a050367fd2d2
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-12 19:09:06 +0000

    upadate3

--- 01_basics/Date_and_time.js
+++ 01_basics/Date_and_time.js
@@ -6,5 +6,27 @@ let myDate = new Date()
 // console.log(typeof myDate)
 
 
-let myCreatedDate = new Date(2023, 0, 23, 7, 5)
-console.log(myCreatedDate.toDateString())
+let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate2 = new Date("2023-01-14")
+let myCreatedDate2 = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString())
+// console.log(myCreatedDate2.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay()+1);
+
+newDate.toLocaleString('default', {
+    weekday:"long",
+
+})
+
+
