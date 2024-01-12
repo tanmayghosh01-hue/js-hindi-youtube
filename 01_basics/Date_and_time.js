9f9a148ea23d399728d8b89a7b2b851f753b6255
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-12 18:33:21 +0000

    update

---
+++ 01_basics/Date_and_time.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(typeof myDate)
+
+
+let myCreatedDate = new Date(2023, 0, 23, 7, 5)
+console.log(myCreatedDate.toDateString())
