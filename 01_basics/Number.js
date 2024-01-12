d6e53f3827f3e6546a387ed85c6ae03fba2aba56
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-12 10:55:50 +0000

    Number and Math in Js

--- 01_basics/Number.js
+++ 01_basics/Number.js
@@ -4,21 +4,45 @@ const score = 4
 
 const balance = new Number(100)
 
-console.log(balance)
-console.log(typeof balance)
+// console.log(balance)
+// console.log(typeof balance)
 
 const sasuke = balance.toString().length
 
-console.log(sasuke)
+// console.log(sasuke)
 
-console.log(balance.toExponential(2))
+// console.log(balance.toExponential(2))
 
 const otherNumber = 1234.8966
 
-console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toPrecision(3))
 
 const hundreds = new Number(100000)
 
-console.log(hundreds.toLocaleString('en-IN')) 
+// console.log(hundreds.toLocaleString('en-IN')) 
 
-console.log(hundreds.toString())
+// console.log(hundreds.toString())
+
+
+// ********** Maths **************
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(3.7))
+// console.log(Math.min(2, 5, 1, 45))
+// console.log(Math.random()*10 + 1)
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min)
+
+
+
+
+
+
+
