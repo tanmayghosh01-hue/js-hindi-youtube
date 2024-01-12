0ce88b7f48e1fac6c1a4f4fc9345368e1b97da40
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-12 10:36:28 +0000

    update

--- 01_basics/Number.js
+++ 01_basics/Number.js
@@ -1,1 +1,24 @@
 // This is about Numbers in Javascript
+
+const score = 4
+
+const balance = new Number(100)
+
+console.log(balance)
+console.log(typeof balance)
+
+const sasuke = balance.toString().length
+
+console.log(sasuke)
+
+console.log(balance.toExponential(2))
+
+const otherNumber = 1234.8966
+
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = new Number(100000)
+
+console.log(hundreds.toLocaleString('en-IN')) 
+
+console.log(hundreds.toString())
