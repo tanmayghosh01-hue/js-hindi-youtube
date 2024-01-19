9484e3a9760b604f1adbf9cb707a9d7a02d3dd7c
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-19 18:09:18 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,51 @@
+//const tinderUser = new Object() -> Singleton object
+
+const tinderUser = {}  // Non - Singleton Object
+
+
+tinderUser.id ="123abc"
+
+tinderUser.name = "Arjun"
+
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser)
+
+
+const regularUser = {
+
+    email: "some@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "Karan",
+            lastname : "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 = {1: "a", 2: "b"}
+
+const obj2 = {3: "a", 4: "b"}
+
+const obj3 = Object.assign({}, obj1, obj2)
+
+// console.log(obj3)
+
+
+console.log(tinderUser)
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIN'))
+
+
+
+
+
