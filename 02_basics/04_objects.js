37794be6df318452b2128e017014f3764d358edd
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-20 11:20:02 +0000

    update

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -36,10 +36,10 @@ const obj3 = Object.assign({}, obj1, obj2)
 // console.log(obj3)
 
 
-console.log(tinderUser)
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
 
 
