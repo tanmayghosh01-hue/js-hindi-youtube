b7a8a683c9026a8014db007f61fadb3f51a8f9d1
tanmayghosh01-hue <76028563+tanmayghosh01-hue@users.noreply.github.com>
2024-01-27 16:52:42 +0000

    function update

--- 03_basics/01_Funtions.js
+++ 03_basics/01_Funtions.js
@@ -9,7 +9,7 @@ function sayMyName() {
 
 // Reference ----------------
 
-sayMyName() // --> execution
+// sayMyName() // --> execution
 
 
 // function addTwoNumbers(number1, number2) {
@@ -25,7 +25,7 @@ function addTwoNumbers(number1, number2) {
 
 const result = addTwoNumbers(2,3)
 
-console.log("Result ", result)
+// console.log("Result ", result)
 
 function loginUserMessage(username="Sam") {
     if(username === undefined){
@@ -35,4 +35,36 @@ function loginUserMessage(username="Sam") {
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 500, 700,2000));
+
+
+const user = {
+    username : "tanmay",
+    prices : 20000
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+handleObject(user)
+
+
+// handleObject({
+//     username: "saumya",
+//     price : 399
+// })
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray))
